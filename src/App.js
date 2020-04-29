f28f74bbfffa9ab5801f1f63b074b33e9732f726
Netanel <netanel@kth.se>
2020-04-30 00:24:23 +0200

    barebones but wokring display screen

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
+import ReactPlayer from 'react-player'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let video = {url:''}
+
+class App extends Component{
+  constructor(){
+    super()
+    this.Pol()
+    this.state = {
+      url:'',
+      order:'',
+    }
+  }
+  render(){
+    return <this.Video/>
+  }
+  
+  Video = () =>{
+    return <ReactPlayer url={/*this.state.url*/'https://www.youtube.com/watch?v=pcPi4jPAR2c'} playing={true} onEnded={this.creditRemove}/> 
+  }
+
+  Pol = () =>{
+    fetch('https://staging-iot-backend.herokuapp.com/video/1')
+    .then(resp => resp.json())
+    .then(data => this.setState({
+      ...this.state,
+      url:data.video.url,
+      order:data.video.order
+    }))
+
+  }
+
+  creditRemove = () =>{
+    console.log(this.state)
+    fetch(`https://staging-iot-backend.herokuapp.com/views/1/${this.state.videoID}/${this.state.order}`,{method:'POST'})
+    this.Pol();
+    window.location.pathname ="/"
+  }
 }
 
 export default App;
