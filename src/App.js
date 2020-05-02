1b375776cb4c8a4cd63f3117cfc2c28d2b2a4422
Magnus <krummelur@gmail.com>
2020-05-02 12:11:26 +0200

    fetch, remove credits, transitions, error handling, only login remains

--- src/App.js
+++ src/App.js
@@ -1,44 +1,160 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import ReactPlayer from 'react-player'
 import './App.css';
 
-let video = {url:''}
+const LOGIN = "LOGIN";
+const IDLE = "IDLE";
+const PLAYING = "PLAYING";
+const baseUrl = "https://staging-iot-backend.herokuapp.com"
+const POLL_TIME = 5000;
 
-class App extends Component{
-  constructor(){
+class App extends Component {
+  constructor() {
     super()
-    this.Pol()
     this.state = {
-      url:'',
-      order:'',
+      appState: LOGIN,
+      url: '',
+      order: '',
     }
   }
-  render(){
-    return <this.Video/>
+
+  componentDidMount() {
+    this.screensaver = window.document.getElementById("screensaver");
+    //For some reason video player does not autoplay reliably if first play is too fast.
+    if (this.getCurrentId() != null) {
+      setTimeout(() => {this.setState({ ...this.state, appState: IDLE })}, 1);
+    }
   }
   
-  Video = () =>{
-    return <ReactPlayer url={/*this.state.url*/'https://www.youtube.com/watch?v=pcPi4jPAR2c'} playing={true} onEnded={this.creditRemove}/> 
+  componentDidUpdate(_, prevState) {
+    if (prevState.appState != this.state.appState) {
+      this.changeState(this.state.appState);
+    }
   }
 
-  Pol = () =>{
-    fetch('https://staging-iot-backend.herokuapp.com/video/1')
+  hideScreensaver() {
+    this.screensaver.classList.remove("fadein");
+    this.screensaver.classList.add("fadeout");
+  }
+
+  showScreenSaver() {
+      this.screensaver.classList.remove("fadeout");
+      this.screensaver.classList.add("fadein");
+  }
+
+  onProgress = (progress) => {
+  //start fading out at < 2 seconds left
+    let timeleftSec = -(progress.playedSeconds - progress.playedSeconds/progress.played);
+    if (timeleftSec < 2.0) 
+      this.showScreenSaver();
+  }
+
+  onEnded = () => {
+    this.creditRemove()
+    .catch(e => {
+        console.error("Error on removing credits, see console:");
+        console.error(e);
+    })
+    .finally(() => {
+      this.setState({ ...this.state, appState: IDLE })
+    })
+  }
+
+  changeState(appState) {
+    console.log("TRANSITIONING TO " + appState);
+    switch (this.state.appState) {
+      case IDLE: 
+        setTimeout(() => this.Poll(), 4000);
+        this.showScreenSaver();
+        break;
+      case PLAYING: 
+        this.hideScreensaver();
+        break;
+      case LOGIN: return
+      default: return
+    }
+  }
+
+  
+  getCurrentId() {
+    return 1;
+    //return window.localStorage.getItem("id");
+  }
+
+  appInnerWindow() {
+    switch (this.state.appState) {
+      case IDLE: return <this.idle/>
+      case PLAYING: return <this.video/>
+      case LOGIN: return <div>UNIMPLEMENTED</div>
+      default: return <this.error/>
+    }
+  }
+
+  render =  this.appInnerWindow;
+
+  error = () => <div style={{color: "red", fontSize:"21px"}}>ERROR NO STATE</div>
+
+  appOverlay = () => <div className="app-overlay"/>
+
+  idle = () => <div className="idle-container"/>
+
+  video = () => {
+    return (
+      <div className="video-container">
+        <div className="video-player-box">
+          <ReactPlayer 
+            width="60%" 
+            height="100%" 
+            url={this.state.video.url} 
+            playing={true}
+            onEnded={() => {this.onEnded()}}
+            onProgress = {p => this.onProgress(p)}
+            />
+        </div>
+      </div>
+    )
+  }
+
+  Poll = () => {
+    let pollOnTimeout = () =>  setTimeout(
+      this.Poll,
+      POLL_TIME);  
+    fetch(`${baseUrl}/video/${this.getCurrentId()}`)
     .then(resp => resp.json())
-    .then(data => this.setState({
-      ...this.state,
-      url:data.video.url,
-      order:data.video.order
-    }))
-
+    .then(data => {
+      if (!data || !data.video || data.video == null) {
+        if(!data)
+          console.error("There was an unexpected response from server (no response)")
+        return pollOnTimeout();
+      } 
+      else {
+        this.setState({
+          ...this.state,
+          appState: "PLAYING",
+          video: data.video
+        })
+      }
+    })
+    .catch(e => {
+      console.error("Error when polling server:");
+      console.error(e);
+      return pollOnTimeout();
+    })
   }
 
-  creditRemove = () =>{
-    console.log(this.state)
-    fetch(`https://staging-iot-backend.herokuapp.com/views/1/${this.state.videoID}/${this.state.order}`,{method:'POST'})
-    this.Pol();
-    window.location.pathname ="/"
+  creditRemove = () => {
+    return fetch(`${baseUrl}/views/${this.getCurrentId()}/${this.state.video.videoId}/${this.state.video.order}`, { method: 'POST' })
+    .then(resp => {
+        if (resp.status != 200)
+          console.error("Unexpected return status from remove credits: " + resp.status);
+        return resp.json();
+    })
+    .catch(e => {
+      console.error("Error when removing credits:");
+      console.error(e);
+      return Promise.reject("Error");
+    })
+    }
   }
-}
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,8 +4,23 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+setTimeout(() => {
+  for(let i = 0; i < 3; i++) {
+    for(let j = 0; j < 4; j++) {
+      window.document.getElementById("circle" + i).style.setProperty('--w' + j,( Math.random() * (window.innerWidth - 100)).toFixed(0) + "px");
+      window.document.getElementById("circle" + i).style.setProperty('--h' + j,( Math.random() * (window.innerHeight - 100)).toFixed(0) + "px");
+    }
+  }
+},
+50);
+
 ReactDOM.render(
   <React.StrictMode>
+    <div className="screensaver-container" id="screensaver">
+      <div className="circle c0" id="circle0"/>
+      <div className="circle c1" id="circle1"/>
+      <div className="circle c2" id="circle2"/>
+    </div>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
